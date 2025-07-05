76c0ce6499bd6f93b2f24c10d8aa70c667fb37e9
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2025-07-05 13:24:17 +0200

    Add files via upload

---
+++ custom_components/entity_broadcaster/const.py
@@ -0,0 +1,13 @@
+"""Constants for the Entity Broadcaster integration."""
+
+DOMAIN = "entity_broadcaster"
+
+# Configuration keys
+CONF_ENTITIES = "entities"
+CONF_UDP_PORT = "udp_port"
+CONF_NAME = "name"
+
+# Default values
+DEFAULT_UDP_PORT = 8888
+MIN_UDP_PORT = 1024
+MAX_UDP_PORT = 65535
